d965683c27043370e8fb468eb974b748828e27d9
Klymenko Illia <klymenko.illia.dev@gmail.com>
2024-03-14 15:36:12 +0200

    bugfix: added len check to text_blocks_set to send an error if the set is empty

--- aws-python-flask-dynamodb-api-project/app.py
+++ aws-python-flask-dynamodb-api-project/app.py
@@ -113,6 +113,18 @@ def process_file(event, context):
     text_blocks = [block['Text'] for block in response['Blocks'] if block['BlockType'] == 'LINE']
     text_blocks_set = set(text_blocks)
 
+    if len(text_blocks_set) == 0:
+        http = urllib3.PoolManager()
+        callback_url = result.get('callback_url', {}).get('S', '').strip()
+        response = http.request(
+            'POST',
+            callback_url,
+            body=json.dumps({"file_id": key, 'error': 'Error with detecting text in document'}),
+            headers={"Content-Type": "application/json"},
+            retries=False
+        )
+        return jsonify({'error': str(e)}), 404
+
     # Prepare item for DynamoDB update
     item = {
         'file_id': {'S': key},
